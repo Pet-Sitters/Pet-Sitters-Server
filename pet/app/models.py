eed53082ee0482ba3167dea654fda0421a7a05db
equestriann <x.dormostuk@gmail.com>
2024-05-29 21:39:05 +0300

    django User model extended

--- pet/app/models.py
+++ pet/app/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+    """ Extended Django built-in User model"""
+
+    tg_name = models.CharField(max_length=15, blank=False)
+    is_sitter = models.BooleanField(default=False, blank=False)
+
+
+class Sitter(models.Model):
+    pass
+
+
+class Owner(models.Model):
+    pass
+
+
+class Admin(models.Model):
+    pass
+
+
+class Keep(models.Model):
+    pass
+
+
+class Feedback(models.Model):
+    pass
