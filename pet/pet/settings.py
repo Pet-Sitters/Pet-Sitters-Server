7362afd869e00287f69a16af8ca04fac693a946d
Vachrameev Oleg <vachrameev.oleg@yandex.ru>
2024-05-31 22:42:39 +0500

    Add registration and swagger

--- pet/pet/settings.py
+++ pet/pet/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'djoser',
+    'drf_spectacular',
     'app',
 ]
 
@@ -123,3 +125,18 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+    ),
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
+
+SPECTACULAR_SETTINGS = {
+    'TITLE': 'API для тестирования',
+    'DESCRIPTION': 'Pet-sitters приложение для ваших мохнатых',
+    'VERSION': '0.1.0',
+}
+
