3b2658bbf7295992ee1f03066a635084b7a61941
equestriann <x.dormostuk@gmail.com>
2024-05-31 16:18:55 +0300

    Sitter & Owner models

--- pet/app/models.py
+++ pet/app/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-from .extensions import CITIES
+from .extensions import CITIES, ANIMALS, SPECIES, GENDER
 
 class User(AbstractUser):
     """ Extended Django built-in User model"""
@@ -13,11 +13,39 @@ class User(AbstractUser):
 
 
 class Sitter(models.Model):
-    pass
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True)
+    birth_date = models.DateField(null=True)
+    social = models.URLField(null=True)
+    about = models.TextField(null=True)
+    animals = models.CharField(choices=ANIMALS, default='NO', max_length=3)
+    passport = models.IntegerField(null=True)
+    ppt_img = models.ImageField(upload_to='passports/', null=True)
+    avatar = models.ImageField(upload_to='avatars/', null=True)
+    rating = models.FloatField(default=0.0)
+    game = models.BooleanField(default=False)
+    activated = models.BooleanField(default=False)
+
+
+class Pet(models.Model):
+    species = models.CharField(choices=SPECIES, default='CAT', max_length=3)
+    breed = models.CharField(null=True, max_length=150)
+    name = models.CharField(max_length=15, null=True)
+    gender = models.CharField(choices=GENDER, default='MAL', max_length=3)
+    sterilized = models.BooleanField(default=True)
+    birth_year = models.DateField(null=True)
+    weigth = models.FloatField(default=0.0)
+    immunized = models.BooleanField(default=True)
+    vet_ppt = models.BooleanField(default=True)
+    emergency_contact = models.CharField(max_length=250, null=True)
+    disease = models.BooleanField(default=False)
+    features = models.TextField(max_length=700, null=True)
 
 
 class Owner(models.Model):
-    pass
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True)
+    pet = models.ForeignKey(Pet, on_delete=models.CASCADE, null=True)
+    rating = models.FloatField(default=0.0)
+    notes = models.TextField(max_length=500, null=True)
 
 
 class Admin(models.Model):
--- pet/pet/settings.py
+++ pet/pet/settings.py
@@ -125,3 +125,5 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'app.user'
+
+MEDIA_URL = '/media/'
