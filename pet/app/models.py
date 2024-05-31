ef6f11a3539a97fb3e12efe4b2caca7f91772499
equestriann <x.dormostuk@gmail.com>
2024-05-31 14:50:38 +0300

    otc, city, address fields added into User model

--- pet/app/models.py
+++ pet/app/models.py
@@ -1,11 +1,15 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from .extensions import CITIES
 
 class User(AbstractUser):
     """ Extended Django built-in User model"""
 
+    otc = models.CharField(max_length=30, null=True, blank=True)
     tg_name = models.CharField(max_length=15, blank=False)
     is_sitter = models.BooleanField(default=False, blank=False)
+    city = models.CharField(choices=CITIES, default='EVN', max_length=3)
+    address = models.CharField(max_length=150, null=True)
 
 
 class Sitter(models.Model):
