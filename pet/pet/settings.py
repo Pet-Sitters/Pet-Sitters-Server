c21002123e4b9d542939c85e1aa0ed128694914e
Vachrameev Oleg <vachrameev.oleg@yandex.ru>
2024-06-03 02:29:29 +0500

    Add serializer and CRU for User fix change name modl PD to Passport

--- pet/pet/settings.py
+++ pet/pet/settings.py
@@ -81,23 +81,23 @@ WSGI_APPLICATION = 'pet.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
-#       'default': {
-#           'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#           'NAME': 'myproject',
-#           'USER': 'postgresuser',
-#           'PASSWORD': 'postgres',
-#           'HOST': 'localhost',
-#           'PORT': '5432',
-#       }
-#   }
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+DATABASES = {
+      'default': {
+          'ENGINE': 'django.db.backends.postgresql_psycopg2',
+          'NAME': 'myproject',
+          'USER': 'postgresuser',
+          'PASSWORD': 'postgres',
+          'HOST': 'localhost',
+          'PORT': '5432',
+      }
+  }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
