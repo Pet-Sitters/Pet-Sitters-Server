4ba4bd21b3833a1d91168ea83932bd438984ee6d
Vachrameev Oleg <vachrameev.oleg@yandex.ru>
2024-06-07 02:37:20 +0500

    change username for postgres database

--- pet/pet/settings.py
+++ pet/pet/settings.py
@@ -81,12 +81,6 @@ WSGI_APPLICATION = 'pet.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 
 DATABASES = {
     'default': {
@@ -99,7 +93,7 @@ DATABASES = {
 #       'default': {
 #           'ENGINE': 'django.db.backends.postgresql_psycopg2',
 #           'NAME': 'myproject',
-#           'USER': 'postgresuser',
+#           'USER': 'myprojectuser',
 #           'PASSWORD': 'postgres',
 #           'HOST': 'localhost',
 #           'PORT': '5432',
