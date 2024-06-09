f1b2a33915f905ab7af91bd8cf2d07e3df11ed8d
equestriann <x.dormostuk@gmail.com>
2024-06-09 20:34:14 +0300

    pet queryset filtered by owners

--- pet/pet_api/views.py
+++ pet/pet_api/views.py
@@ -2,11 +2,11 @@ from rest_framework import viewsets, response
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 from .serializers import PetSerializer
-from app.models import Pet
+from app.models import Pet, Owner
 
 
 class PetSerializerAPI(viewsets.ModelViewSet):
-    queryset = Pet.objects.all()
+    # queryset = Pet.objects.all()
     serializer_class = PetSerializer
     permission_classes = [IsAuthenticated]
 
@@ -15,3 +15,12 @@ class PetSerializerAPI(viewsets.ModelViewSet):
         if serializer.is_valid():
             serializer.save(user=request.user.id)
             return response.Response(request.data)
+
+    def get_queryset(self):
+        user = self.request.user.id
+        try:
+            owner = Owner.objects.get(user=user)
+            pet_list = Pet.objects.filter(owner=owner)
+            return pet_list
+        except:
+            return response.Response({'error': 'This user is not an owner yet!'})
