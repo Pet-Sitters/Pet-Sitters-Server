02e8e2b7cb2f9d958a44d8af179918f1ea19824b
equestriann <x.dormostuk@gmail.com>
2024-06-18 14:33:22 +0300

    Sitter create: photos disabled, passport auto related

--- pet/sitter/serializers.py
+++ pet/sitter/serializers.py
@@ -4,11 +4,11 @@ from app.serializers import UserSerializer
 from drf_writable_nested import WritableNestedModelSerializer
 
 
-class HomeImagesSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = HomeImages
-        fields = ['image']
+# class HomeImagesSerializer(serializers.ModelSerializer):
+#
+#     class Meta:
+#         model = HomeImages
+#         fields = ['image']
 
 
 class PassportSerializer(serializers.ModelSerializer):
@@ -19,10 +19,38 @@ class PassportSerializer(serializers.ModelSerializer):
 
 
 class SitterSerializer(WritableNestedModelSerializer, serializers.ModelSerializer):
-    user = UserSerializer()
-    images = HomeImagesSerializer(many=True)
+    # user = UserSerializer()
     passport = PassportSerializer()
 
     class Meta:
         model = Sitter
-        exclude = ['rating', 'game', 'activated']
+        fields = [
+            # "user",
+            "first_name",
+            "last_name",
+            "patronym",
+            "tg_nick",
+            "tg_id",
+            "phone_num",
+            "city",
+            "address",
+            "birth_date",
+            "social",
+            "about",
+            "home",
+            "animals",
+            "rating",
+            "game",
+            "activated",
+            "passport"
+        ]
+
+        def create(self, validated_data):
+            print(validated_data)
+            cur_user = validated_data.pop('user')
+            pprt_data = validated_data.pop('passport')
+
+            new_sitter = Sitter.objects.create(user=cur_user, **validated_data)
+            sitter_passport = Passport.objects.create(sitter=new_sitter, **pprt_data)
+
+            return new_sitter, sitter_passport
--- pet/sitter/views.py
+++ pet/sitter/views.py
@@ -1,4 +1,5 @@
-from rest_framework import viewsets, response
+from rest_framework import viewsets, status
+from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from .serializers import SitterSerializer
 from app.models import Sitter
@@ -7,4 +8,9 @@ from app.models import Sitter
 class SitterSerializerAPI(viewsets.ModelViewSet):
     queryset = Sitter.objects.all()
     serializer_class = SitterSerializer
-    permission_classes = [IsAuthenticated]
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save(user=request.user)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
